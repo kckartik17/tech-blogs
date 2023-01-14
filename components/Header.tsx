5d58c2ca59698a0831347178968caff56ae53d6d
Kartik Chauhan <kckartik17@gmail.com>
2023-01-14 21:53:57 +0530

    logo color change
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { GitHub } from "react-feather";
-import logo from "../public/logo.png";
+import logo from "../public/logo.svg";
 function Header() {
   return (
     <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
